822529a288e8167efbe5fb3f5dd96922ac77cc30
smithosagie <smithosagie24@gmail.com>
2025-03-13 20:35:38 -0500

    Jia story 0000: Apply FlowRow to allow excess text to row over to the next line

--- app/src/main/java/com/example/walmartapp/ui/screens/CountryDataLayout.kt
+++ app/src/main/java/com/example/walmartapp/ui/screens/CountryDataLayout.kt
@@ -1,10 +1,9 @@
 package com.example.walmartapp.ui.screens
 
 import androidx.compose.foundation.BorderStroke
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.ExperimentalLayoutApi
+import androidx.compose.foundation.layout.FlowRow
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Card
@@ -18,13 +17,14 @@ import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.unit.dp
 import com.example.walmartapp.model.Country
 
+@OptIn(ExperimentalLayoutApi::class)
 @Composable
 fun CountryDataLayout(country: Country) {
     country.let {
         Card(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(vertical = 6.dp)
+                .padding(vertical = 6.dp, horizontal = 4.dp)
                 .testTag("country_card"),
             elevation = CardDefaults.cardElevation(4.dp),
             border = BorderStroke(width = 2.dp, color = Color.Black),
@@ -34,34 +34,37 @@ fun CountryDataLayout(country: Country) {
         ) {
             Column(modifier = Modifier
                 .testTag("country_column")) {
-                Row(modifier = Modifier
-                    .testTag("country_column")) {
+                FlowRow(
+                    modifier = Modifier
+                        .testTag("country_flow_row")
+                        .padding(8.dp),
+                ) {
                     Text(
+                        modifier = Modifier
+                            .padding(horizontal = 4.dp, vertical = 4.dp),
                         text = "${it.name},",
-                        style = MaterialTheme.typography.titleLarge,
-                        modifier = Modifier
-                           .padding(horizontal = 8.dp, vertical = 8.dp),
+                        style = MaterialTheme.typography.titleLarge
                     )
                     Text(
+                        modifier = Modifier
+                            .padding(start = 8.dp, end = 4.dp, top = 4.dp, bottom = 4.dp),
                         text = it.region,
-                        style = MaterialTheme.typography.titleMedium,
-                        modifier = Modifier
-                          .padding(horizontal = 8.dp, vertical = 8.dp),
+                        style = MaterialTheme.typography.titleLarge
                     )
                     Text(
-                        text = " ${it.code}",
-                        style = MaterialTheme.typography.titleMedium,
                         modifier = Modifier
-                            .padding(horizontal = 8.dp, vertical = 8.dp),
+                            .padding(horizontal = 4.dp, vertical = 4.dp),
+                        text = " ${it.code}",
+                        style = MaterialTheme.typography.titleLarge
                     )
                 }
+            }
                 Text(
                     text = " ${it.capital}",
                     style = MaterialTheme.typography.titleLarge,
                     modifier = Modifier
-                      .padding(horizontal = 4.dp),
+                      .padding(horizontal = 4.dp, vertical = 8.dp),
                 )
             }
         }
-    }
 }
